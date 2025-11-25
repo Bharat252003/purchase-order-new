e8b426ec5558855f8d06505335b53bc9a5bbb5d2
Bharat Prajapati <Bharatprajapati2503@gmail.com>
2025-11-25 17:46:20 +0530

    added CORS config in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   createDocument(app);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://purchase-order-frontend.vercel.app', // add your deployed Next.js domain
+    ],
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   await app.listen(port);
   // eslint-disable-next-line no-console
   console.log(`Server running on http://localhost:${port}`);
